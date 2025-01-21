44d9efe9dcfa578627c63b2452cf47afa7d03e7e
Luis Vallejos <luisvallstudio@gmail.com>
2025-01-21 14:01:01 -0300

    se cambio el favicon

--- src/components/storeComponents/CartContext.jsx
+++ src/components/storeComponents/CartContext.jsx
@@ -1,9 +1,24 @@
 import { createContext, useState } from "react";
 
 export const CartContext = createContext();
+
+const initialState = [];
+const reduce = (state, action) => {
+  switch (action.type) {
+    case "add_to_cart": {
+      console.log("Agregando");
+    }
+    case "remove_to_cart": {
+      console.log("Eliminando");
+    }
+    case 'clean_cart': {
+      console.log('Limpiando')
+    }
+  }
+};
+
 export function CartProvider({ children }) {
   const [cart, setCart] = useState([]);
-
 
   const addCart = (product) => {
     const productInCartIndex = cart.findIndex((item) => item.id === product.id);
@@ -22,17 +37,17 @@ export function CartProvider({ children }) {
   };
 
   const DtlProduct = (product) => {
-    const porductToDelete = cart.findIndex((item) => item.id === product.id)
-    const newCartToDelete = structuredClone(cart)
+    const porductToDelete = cart.findIndex((item) => item.id === product.id);
+    const newCartToDelete = structuredClone(cart);
 
-    if (product.quantity > 1){
+    if (product.quantity > 1) {
       newCartToDelete[porductToDelete].quantity--;
-      setCart(newCartToDelete)
+      setCart(newCartToDelete);
     } else {
-      newCartToDelete.splice(porductToDelete,1)
-      setCart(newCartToDelete)
+      newCartToDelete.splice(porductToDelete, 1);
+      setCart(newCartToDelete);
     }
-  }
+  };
 
   return (
     <CartContext.Provider
