c9a45a90edfb3b2c38f04bd819bf848f2b89fcc6
Luis Vallejos <luisvallstudio@gmail.com>
2025-01-21 23:24:51 -0300

    se cmabió el token de autenticación

--- src/components/LogContext.jsx
+++ src/components/LogContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useState } from "react";
 export const LogContext = createContext();
 
 export function LogProvider({children}) {
-  const [isLoged, setIsLoged] = useState(!false);
+  const [isLoged, setIsLoged] = useState(false);
   return (
     <LogContext.Provider value={{ isLoged, setIsLoged }}>
       {children}
